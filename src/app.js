02cb3c88e78760739c6c2e8ab56ccb9eab6c6782
mujtabahs <mujtaba.hussein@gmail.com>
2022-05-28 14:34:59 +0500

    second commit

--- src/app.js
+++ src/app.js
@@ -6,7 +6,6 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
-// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
