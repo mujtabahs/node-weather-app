a3a87e71b3d256684c17faa5297c49165afe5c58
mujtabahs <mujtaba.hussein@gmail.com>
2022-05-28 15:32:45 +0500

    Updated with weatherstack and mapbox

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const forecast = require('./utils/forecast')
 const app = express()
 const port = process.env.PORT || 3000
 
-
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
 const viewsPath = path.join(__dirname, '../templates/views')
 const partialsPath = path.join(__dirname, '../templates/partials')
@@ -98,5 +98,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log('Server is up on port '+port)
+    console.log('Server is up on port ' + port)
 })
